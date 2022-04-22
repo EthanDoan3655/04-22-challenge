d0eb213801a364235573bf38c96091474d390859
Ethan Doan <ethandoan10@gmail.com>
2022-04-22 13:54:07 -0400

    please

---
+++ Fcc-java.js
@@ -0,0 +1,25 @@
+//1) Create 3 arrays, One with all String data types, one with all Numbers data types, and one with Boolean data types.  You will need to add sample data to each array (don't over think this). Each array should have at least 5 elements.. 
+
+//2) Next create a function that can take any of these arrays as a parameter and add an additional String, Number, or Boolean depending on which array was passed in. 
+
+//xHint: You will need an array method to add new items to the end of the array. You will also need conditionals set up to check the item at index 0 so you know what array was passed into the function.  You can use the typeof method to determine what type of data is in the array.  
+
+let classicBmws = ['e30','e36','e46','e90','f80'];
+
+console.log(classicBmws);
+
+
+ let favoriteNumber =[1,2,3,4,5];
+
+ console.log(favoriteNumber);
+
+ let a = correct;
+ let b = wrong;
+ let c = maybe;
+ let d = ehhh;
+
+ if (a){
+     console.log("a is correct")
+ } else {
+     console.log("a is wrong")
+ }
